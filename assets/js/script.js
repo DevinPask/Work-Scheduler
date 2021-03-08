06f9e1869e5c1cb31c11c0e0a16747feed5cc686
Dipper Pines <69230516+DevinPask@users.noreply.github.com>
2021-03-07 17:31:06 -0800

    Color coding fixed,minus button, styling

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,12 +6,6 @@ const dateHour = moment().format("h");
 console.log(dateHour);
 
 
-// timeblocks for standard business hours
-// $(".edit").on("click", function () {
-//     var status = document.getElementById("todoText").isContentEditable;
-//     $("#todoText").prop("contenteditable", !status);
-// });
-
 $(".addButton").on("click", function () {
     // Get nearby values of the description in JQuery
     var text = $(this).siblings(".description").val();
@@ -20,37 +14,53 @@ $(".addButton").on("click", function () {
     // Save text in local storage
     localStorage.setItem(time, text);
 })
-    
+
+$(".clearButton").on("click", function () {
+    var time = $(this).parent().attr("id");
+
+    // Save text in local storage
+    localStorage.removeItem(time);
+
+    applyVals();
+})
 
 function timeTracker() {
-        //get current number of hours.
-        var timeNow = moment().hour();
+    //get current number of hours.
+    var timeNow = moment().hour();
 
-        // loop over time blocks
-        $(".time-block").each(function () {
-            var blockTime = parseInt($(this).attr("id").split("hour")[1]);
+    // loop over time blocks
+    $(".time-block").each(function (index) {
+        var blockTime = parseInt($(this).attr("id").split("hour")[1]);
+        var $todo = $(".todo").eq(index);
 
-            // To check the time and add the classes for background indicators
-            if (blockTime < timeNow) {
-                $(".todo").removeClass("future");
-                $(".todo").removeClass("present");
-                $(".todo").addClass("past");
-            }
-            else if (blockTime === timeNow) {
-                $(".todo").removeClass("past");
-                $(".todo").removeClass("future");
-                $(".todo").addClass("present");
-            }
-            else {
-                $(".todo").removeClass("present");
-                $(".todo").removeClass("past");
-                $(".todo").addClass("future");
+        // To check the time and add the classes for background indicators
+        if (blockTime < timeNow) {
+            $todo.removeClass("future");
+            $todo.removeClass("present");
+            $todo.addClass("past");
+        }
+        else if (blockTime === timeNow) {
+            $todo.removeClass("past");
+            $todo.removeClass("future");
+            $todo.addClass("present");
+        }
+        else {
+            $todo.removeClass("present");
+            $todo.removeClass("past");
+            $todo.addClass("future");
 
-            }
-        })
+        }
+    })
+}
+
+// Get item from local storage if any
+function applyVals() {
+    for (var i in localStorage) {
+        if (localStorage.hasOwnProperty(i)) {
+            // arrayOfValues.push(localStorage[i]);
+            console.log(i, localStorage[i]);
+        }
     }
-
-    // Get item from local storage if any
     $("#hour9 .description").val(localStorage.getItem("hour9"));
     $("#hour10 .description").val(localStorage.getItem("hour10"));
     $("#hour11 .description").val(localStorage.getItem("hour11"));
@@ -60,27 +70,9 @@ function timeTracker() {
     $("#hour15 .description").val(localStorage.getItem("hour15"));
     $("#hour16 .description").val(localStorage.getItem("hour16"));
     $("#hour17 .description").val(localStorage.getItem("hour17"));
+}
 
-    timeTracker();
-
-
-
-// Get value from element on input events
+timeTracker();
+applyVals();
 
 
-// Set some value
-// WHEN I view the timeblocks for that day
-// THEN each timeblock is color coded to indicate whether it is in the past, present, or future
-
-    // Compare row id to current hour and sets color accordingly
-
-
-// WHEN I click into a timeblock
-// THEN I can enter an event
-
-// WHEN I click the save button for that timeblock
-// THEN the text for that event is saved in local storage
-
-
-// WHEN I refresh the page
-// THEN the saved events persist
